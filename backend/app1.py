f256f001faf0cad94e75a310394a25914a80a4f9
16gsanket <16gsanket@gmail.com>
2024-04-22 20:09:06 +0530

    desktop push

--- backend/app1.py
+++ backend/app1.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+import pandas as pd
 import joblib
 
 app = Flask(__name__)
@@ -11,17 +12,32 @@ model = joblib.load('isolation_forest_model.pkl')
 # Define endpoint for anomaly detection
 @app.route('/detect-anomalies', methods=['POST'])
 def detect_anomalies():
-    # Get the data from the request
-    data = request.json
+    if 'file' not in request.files:
+        return jsonify({'error': 'No file part'})
     
-    # Process the data (you can pass it through your trained model)
-    # Here you'll need to adapt this part according to how your model is used
+    file = request.files['file']
     
-    # For example, if your model expects a certain format of input data:
-    # result = model.predict(data)
+    if file.filename == '':
+        return jsonify({'error': 'No selected file'})
     
-    # Return the result
-    return jsonify({'result': 'anomaly detection result'})
+    if file:
+        try:
+            # Read CSV file
+            df = pd.read_csv(file)
+
+               # Read the CSV file into a DataFrame
+            df = pd.read_csv(file, delimiter=';')
+            
+            # Extract relevant features from the dataset
+            features = df[['like', 'share', 'comment']]  # Adjust this according to your dataset
+            
+            # Process the data (pass it through your trained model)
+            result = model.predict(features)
+            
+            # Return the result
+            return jsonify({'result': result.tolist()})
+        except Exception as e:
+            return jsonify({'error': str(e)})
 
 if __name__ == '__main__':
     app.run(debug=True)
